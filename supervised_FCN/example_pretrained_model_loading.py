191451ecf3c54f91b82bb43963336405a57ea466
dslee <daesoolee2601@gmail.com>
2022-08-17 08:13:42 +0200

    simplified example_pretrained_model_loading.py

--- supervised_FCN/example_pretrained_model_loading.py
+++ supervised_FCN/example_pretrained_model_loading.py
@@ -1,13 +1,19 @@
 import os
+import pandas as pd
 import torch
 from supervised_FCN.models.fcn import FCNBaseline
 
 
-if __name__ == '__main__':
-    # config
-    subset_dataset_name = 'Adiac'
-    in_channels = 1  # univariate model
-    n_classes = 37  # can be found here [https://www.cs.ucr.edu/~eamonn/time_series_data_2018/]
+def load_pretrained_FCN(subset_dataset_name: str, in_channels: int = 1):
+    """
+    load a pretrained FCN (Fully Convolutional Network)
+    :param subset_dataset_name:
+    :param in_channels: 1; univariate for the UCR subset datasets.
+    :return:
+    """
+    ucr_summary = pd.read_csv(os.path.join('datasets', 'DataSummary_UCR.csv'))
+    q = ucr_summary.query(f"Name == '{subset_dataset_name}'")
+    n_classes = q['Class'].iloc[0]
 
     # build
     fcn = FCNBaseline(in_channels, n_classes)
@@ -15,5 +21,9 @@ if __name__ == '__main__':
     # load
     ckpt_fname = os.path.join('saved_models', f'{subset_dataset_name}.ckpt')
     fcn.load_state_dict(torch.load(ckpt_fname))
+    return fcn
 
+
+if __name__ == '__main__':
+    fcn = load_pretrained_FCN('Adiac')
     print(fcn)
