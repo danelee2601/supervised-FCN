1d13c1d1956eb93e8811f32ac065473547107442
dslee <daesoolee2601@gmail.com>
2022-09-02 19:37:26 +0200

    version number update

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ install_reqs = [f'{item["name"]}=={item["version"]}' for item in install_reqs]
 
 setup(
     name="supervised-fcn",  # pypi 에 등록할 라이브러리 이름
-    version="1.3.0",  # pypi 에 등록할 version (수정할 때마다 version up을 해줘야 함)
+    version="1.4.0",  # pypi 에 등록할 version (수정할 때마다 version up을 해줘야 함)
     description="it provides a pretrained FCN (Fully Convolutional Network).",
     author="Daesoo Lee",
     author_email="daesoolee2601@gmail.com",
