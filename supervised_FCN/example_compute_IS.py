c44f3d20aec2b16620759f4351655819983a48f2
dslee <daesoolee2601@gmail.com>
2022-09-02 19:35:59 +0200

    update `n_split` in `calculate_inception_score`

--- supervised_FCN/example_compute_IS.py
+++ supervised_FCN/example_compute_IS.py
@@ -9,6 +9,7 @@ reference: https://machinelearningmastery.com/how-to-implement-the-inception-sco
 [2] Smith, Kaleb E., and Anthony O. Smith. "Conditional GAN for timeseries generation." arXiv preprint arXiv:2006.16477 (2020)
 """
 # calculate inception score in numpy
+import numpy as np
 from numpy import asarray
 from numpy import expand_dims
 from numpy import log
@@ -17,35 +18,53 @@ from numpy import exp
 
 
 # calculate the inception score for p(y|x)
-def calculate_inception_score(p_yx, eps=1e-16):
-    # calculate p(y)
-    p_y = expand_dims(p_yx.mean(axis=0), 0)
+def calculate_inception_score(P_yx, n_split: int = 10, shuffle: bool = True, eps: float = 1e-16):
+    """
+    P_yx: (batch_size dim)
+    """
+    if shuffle:
+        np.random.shuffle(P_yx)  # in-place
 
-    # kl divergence for each image
-    kl_d = p_yx * (log(p_yx + eps) - log(p_y + eps))  # KL divergence = p(y|x) * (log(p(y|x)) – log(p(y)))
+    scores = list()
+    n_part = int(np.floor(P_yx.shape[0] / n_split))
+    for i in range(n_split):
+        # retrieve p(y|x)
+        ix_start, ix_end = i * n_part, i * n_part + n_part
+        p_yx = P_yx[ix_start:ix_end]
 
-    # sum over classes
-    sum_kl_d = kl_d.sum(axis=1)
+        # calculate p(y)
+        p_y = expand_dims(p_yx.mean(axis=0), 0)
 
-    # average over images
-    avg_kl_d = mean(sum_kl_d)
+        # calculate KL divergence using log probabilities
+        kl_d = p_yx * (log(p_yx + eps) - log(p_y + eps))
 
-    # undo the logs
-    is_score = exp(avg_kl_d)
-    return is_score
+        # sum over classes
+        sum_kl_d = kl_d.sum(axis=1)
+
+        # average over images
+        avg_kl_d = mean(sum_kl_d)
+
+        # undo the log
+        is_score = exp(avg_kl_d)
+
+        # store
+        scores.append(is_score)
+
+    # average across images
+    is_avg, is_std = np.mean(scores), np.std(scores)
+    return is_avg, is_std
 
 
 if __name__ == '__main__':
-    # we can imagine the case of three classes of image and a perfect confident prediction for each class for three images.
-    # conditional probabilities for high quality images
-    p_yx = asarray([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])  # p(y|x)
+    # assume that we have 50 samples and 3 classes.
+    # generate synthetic `p(y|x)`
+    rand = np.random.rand(50, 3)
+    p_yx = rand / np.sum(rand, axis=1, keepdims=True)  # p(y|x)
 
-    # score
-    score = calculate_inception_score(p_yx)
-    print(score)
+    # calculate inception score
+    is_avg, is_std = calculate_inception_score(p_yx)
+    print(f"""
+    IS (mean): {is_avg}
+    IS (std): {is_std}
+    """)
 
-    # we can also try the worst case.
-    # conditional probabilities for low quality images
-    p_yx = asarray([[0.33, 0.33, 0.33], [0.33, 0.33, 0.33], [0.33, 0.33, 0.33]])
-    score = calculate_inception_score(p_yx)
-    print(score)
